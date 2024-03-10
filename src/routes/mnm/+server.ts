8bacce91f3c141172250b165b54a7f26832d183d
Tamás Vince Kornél <vin@kys.social>
2024-03-10 17:44:38 +0100

    shhh

---
+++ src/routes/mnm/+server.ts
@@ -0,0 +1,7 @@
+// this is for a school project, okay?
+
+import { redirect } from '@sveltejs/kit'
+
+export function GET() {
+  return redirect(302, 'https://im.tvk.lol/share/sifdW2T-xChoBrTMshm_8zEL2S6ym_hkJtNf5gE_ZxD1yg69fMtiCCP7dMKfPBZrzcw')
+}
